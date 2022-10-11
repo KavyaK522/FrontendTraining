f1ae7e4415db2b500a2af3d6a335f5d38b809a38
Kavya K <kk@orthofx.com>
2022-10-11 15:11:37 +0530

    Uptovideo17

--- Angular6Project/src/app/employee/create-employee.component.ts
+++ Angular6Project/src/app/employee/create-employee.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
+import { FormGroup, FormBuilder, Validators, RequiredValidator, AbstractControl } from '@angular/forms';
+import { CustomValidators } from '../shared/custom.validators';
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
@@ -15,7 +15,11 @@ export class CreateEmployeeComponent implements OnInit {
     'maxLength' : 'Full Name must be less than 10 characters.'
    },
    'email' : {
-    'required' : 'Email is required.'
+    'required' : 'Email is required.',
+    'emailDomain' : 'Email domain should be orthofx.com'
+   },
+   'phone' : {
+    'required' : 'Phone is required.'
    },
    'skillName' : {
     'required' : 'Skill Name is required.'
@@ -29,8 +33,9 @@ export class CreateEmployeeComponent implements OnInit {
   };
 
   formErrors = {
-    'fullNAme' : '',
+    'fullName' : '',
     'email' : '',
+    'phone' : '',
     'skillName' : '',
     'experienceInYears' : '',
     'proficiency' : ''
@@ -40,27 +45,48 @@ export class CreateEmployeeComponent implements OnInit {
   ngOnInit(){
     this.employeeForm = this.fb.group({
     fullName: ['',[Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
-    email: ['', Validators.required],
+    contactPreference: ['email'],
+    email: ['', [Validators.required, CustomValidators.emailDomain('orthofx.com')]],
+    phone: ['',Validators.required],
     skills: this.fb.group({
       skillName: ['', Validators.required],
       experienceInYears: ['', Validators.required],
       proficiency: ['', Validators.required]
     })
     });
+
+    this.employeeForm.get('contactPreference')?.valueChanges.subscribe((data: string) => {
+      this.logValidationErrors(this.employeeForm);
+    });
+
+
+    this.employeeForm.valueChanges.subscribe((data) =>{
+      this.logValidationErrors(this.employeeForm);
+    });
   }
-  logValidationErrors(group: FormGroup): void {
+
+  onContactPreferenceChange(selectedValue: string){
+    const phoneControl = this.employeeForm.get('phone');
+    if (selectedValue === 'phone'){
+      phoneControl?.setValidators(Validators.required);
+    } else {
+      phoneControl?.clearValidators();
+    }
+    phoneControl?.updateValueAndValidity();
+  }
+  logValidationErrors(group: FormGroup = this.employeeForm): void {
     Object.keys(group.controls).forEach((key: string) => {
       const abstractControl = group.get(key);
       if(abstractControl instanceof FormGroup){
         this.logValidationErrors(abstractControl);
       } else {
-        this.formErrors[key] = '';
-        if (abstractControl && !abstractControl.valid) {
-          const messages = this.validationMessages[key];
+        (this.formErrors as any)[key] = '';
+        if (abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty)) {
+          const messages = (this.validationMessages as any)[key];
           console.log(messages);
           for (const errorKey in abstractControl.errors){
             if (errorKey) {
-              this.formErrors[key] += messages[errorKey] + ' ';
+              (this.formErrors as any)[key] += messages[errorKey] + ' ';
             }
           }
         }
@@ -77,8 +103,10 @@ export class CreateEmployeeComponent implements OnInit {
 
   }
   onLoadDataClick(): void{
-   this.logValidationErrors(this.employeeForm);
-   console.log(this.formErrors);
+  //  this.logValidationErrors(this.employeeForm);
+  //  console.log(this.formErrors);
   }
 
 }
+
+
